c7473b25aaedad076bc88750d23b7d1d6fe12cfd
Michael Benedict <michaelbt@live.com>
2018-07-24 19:34:13 +1000

    sdm845-common: add target screen height/weight
    Signed-off-by: Michael Benedict <michaelbt@live.com>

--- common.mk
+++ common.mk
@@ -21,6 +21,10 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Get non-open-source specific aspects
 $(call inherit-product-if-exists, vendor/samsung/sdm845-common/sdm845-common-vendor.mk)
 
+# Boot animation
+TARGET_SCREEN_HEIGHT := 2960
+TARGET_SCREEN_WIDTH := 1440
+
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
