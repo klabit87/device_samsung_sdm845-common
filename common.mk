e75ec52628d4fedcb89b7969435d5e3656bece34
Michael Benedict <michaelbt@live.com>
2019-03-09 19:51:08 +1100

    sdm845-common: dont import qti files from device tree
    because we already import it from vendor
    
    Signed-off-by: Michael Benedict <michaelbt@live.com>

--- common.mk
+++ common.mk
@@ -86,8 +86,3 @@ PRODUCT_PACKAGES += \
     NfcNci \
     Tag \
     com.android.nfc_extras
-
-# QTI
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/qti_whitelist.xml:system/etc/sysconfig/qti_whitelist.xml \
-    $(LOCAL_PATH)/permissions/privapp-permissions-qti.xml:system/etc/permissions/privapp-permissions-qti.xml
