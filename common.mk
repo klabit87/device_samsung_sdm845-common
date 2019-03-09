a75cfe2b9271cc82602b403491c14ab16e226e16
Michael Benedict <michaelbt@live.com>
2019-03-09 23:17:10 +1100

    rootdir: update ueventd name
    Signed-off-by: Michael Benedict <michaelbt@live.com>

--- common.mk
+++ common.mk
@@ -50,7 +50,7 @@ TARGET_SCREEN_WIDTH := 1440
 
 # Init
 PRODUCT_PACKAGES += \
-    ueventd.rc
+    ueventd.qcom.rc
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/vndk-compat/vndk-compat.rc:system/etc/init/vndk-compat.rc
--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -1,7 +1,7 @@
 LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE            := ueventd.rc
+LOCAL_MODULE            := ueventd.qcom.rc
 LOCAL_MODULE_TAGS       := optional eng
 LOCAL_MODULE_CLASS      := ETC
 LOCAL_SRC_FILES         := etc/ueventd.qcom.rc
