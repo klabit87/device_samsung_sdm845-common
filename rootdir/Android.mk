b77c92b2880850f3d777c1af717b00ea737c3b91
Michael Benedict <michaelbt@live.com>
2019-03-24 15:51:30 +1100

    rootdir: move init.qcom.rc to root
    Signed-off-by: Michael Benedict <michaelbt@live.com>

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -5,7 +5,7 @@ LOCAL_MODULE       := init.qcom.rc
 LOCAL_MODULE_TAGS  := optional eng
 LOCAL_MODULE_CLASS := ETC
 LOCAL_SRC_FILES    := etc/init.qcom.rc
-LOCAL_MODULE_PATH  := $(TARGET_OUT_ETC)/init
+LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
