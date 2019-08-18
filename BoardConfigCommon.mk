45bd338d9e464ce44198e7ba84e75fae09df0650
klabit87 <keith.labit@gmail.com>
2019-08-17 23:46:31 -0500

    Cleanup BoardConfig
    Change-Id: I47d0d3dcafa262d2d77ce81928d0f9bd0ffc5002

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -47,7 +47,9 @@ BOARD_RAMDISK_OFFSET := 0x02000000
 BOARD_KERNEL_CMDLINE := console=null androidboot.hardware=qcom 
 BOARD_KERNEL_CMDLINE += video=vfb:640x400,bpp=32,memsize=3072000 msm_rtb.filter=0x237
 BOARD_KERNEL_CMDLINE += ehci-hcd.park=3 lpm_levels.sleep_disabled=1 service_locator.enable=1 swiotlb=2048
-BOARD_KERNEL_CMDLINE += androidboot.configfs=true androidboot.usbcontroller=a600000.dwc3 firmware_class.path=/vendor/firmware_mnt/image loop.max_part=7
+BOARD_KERNEL_CMDLINE += androidboot.configfs=true androidboot.usbcontroller=a600000.dwc3 
+BOARD_KERNEL_CMDLINE += firmware_class.path=/vendor/firmware_mnt/image/ loop.max_part=7
+BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
 BOARD_KERNEL_IMAGE_NAME := Image.gz-dtb
 BOARD_KERNEL_PAGESIZE := 4096
 TARGET_KERNEL_ARCH := arm64
